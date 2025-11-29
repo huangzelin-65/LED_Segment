1ff85beb33c9da869167387c873d04d4a0f5ee9b
Jiajie Li <lijj453@midea.com>
2025-11-29 17:47:50 +0800

    解决PB14无法上拉的问题

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -163,6 +163,8 @@ int main(void)
   /* USER CODE BEGIN 2 */
   bEeprom_Adaptor_Init(&hi2c1);
 
+  // PB14 属于 UCPD（USB Type‑C PD）CC 引脚，默认被内部下拉拉低, 此处禁用UCPD的默认下拉
+  SET_BIT(PWR->UCPDR, PWR_UCPDR_UCPD_DBDIS); 
   /* USER CODE END 2 */
 
   /* Init scheduler */
